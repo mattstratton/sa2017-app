422bb0597d8f2fe8d54249062e89649bd1d43f94
Matt Stratton <matt.stratton@gmail.com>
2017-11-28 06:57:45 -0600

    Fix foodcritic errors

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,21 @@
 name 'sa2017-app'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Matt Stratton'
+maintainer_email 'matt.stratton@gmail.com'
+license 'Apache-2.0'
 description 'Installs/Configures sa2017-app'
 long_description 'Installs/Configures sa2017-app'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+supports 'centos'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/sa2017-app/issues'
+issues_url 'https://github.com/mattstratton/sa2017-app/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/sa2017-app'
+source_url 'https://github.com/mattstratton/sa2017-app'
