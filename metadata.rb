87909b840cc5a68a3d5d959f882d4a7d03c5210d
Matt Stratton <matt.stratton@gmail.com>
2017-11-28 19:54:37 -0600

    Version bump to kick off jenkins

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'matt.stratton@gmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures sa2017-app'
 long_description 'Installs/Configures sa2017-app'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'centos'
 
