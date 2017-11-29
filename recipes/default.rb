3bf69d65c25df0bf7d9c1ab0e6d69cc6267e2c34
Matt Stratton <matt.stratton@gmail.com>
2017-11-28 19:47:06 -0600

    Modify /var/log permissions

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+user 'sa2017-app' do
+  comment 'SysAdvent User'
+  system true
+  shell '/bin/false'
+end
+
+
+directory '/var/log' do
+  owner 'sa2017-app'
+end
